5cea8862254767e845baada73f459a04fa0fd44f
jbkaa <git config --global user.email lxttxxx@gmail.comgit config --global user.email>
2022-01-30 11:19:58 +0100

    E32

--- Program.cs
+++ Program.cs
@@ -7,11 +7,19 @@ namespace sda_csharp_exercises
         static void Main(string[] args)
         {
 
-            Rectangle prostokat = new Rectangle(2, 2);
-            prostokat.Show();
+            Rectangle prostokat1 = new Rectangle(2, 2);
+            prostokat1.Show();
             Rectangle.PrintClassName();
-         
 
+            Rectangle[] rectangles = new Rectangle[3];
+            rectangles[0] = prostokat1;
+            rectangles[1] = new Rectangle(4, 5);
+            rectangles[2] = new Rectangle(5, 2);
+
+            foreach(Rectangle rectangle in rectangles)
+            {
+                rectangle.Show();
+            }
 
         }
     }
