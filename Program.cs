d10ee6089df4d126a006b0ca441e3da17076e950
jbkaa <git config --global user.email lxttxxx@gmail.comgit config --global user.email>
2022-01-30 11:09:43 +0100

    E31 commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,19 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Shape shape1 = new Square(5);
+            Console.WriteLine(shape1.GetArea());
+            Shape shape2 = new Circle(5);
+            Console.WriteLine(shape2.GetArea());
+
+            Shape[] shapes = new Shape[2];
+            shapes[0] = shape1;
+            shapes[1] = shape2;
+
+            double area = Shape.GetTotalArea(shapes);
+            Console.WriteLine(area);
+            
+
         }
     }
 }
